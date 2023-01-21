6ed1c330aeeda24c2b471ce7b213d87d0afe4057
Camila Holanda <camilaholanda1997@gmail.com>
2023-01-21 07:32:43 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("#form-habits")
+//puxando biblioteca de fora
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+  alert("Adicionado com sucesso!")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@Habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@Habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
